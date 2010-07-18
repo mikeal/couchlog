c812fe10786271de85bac301fcc06d2264468db9
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-07-18 13:11:14 -0700

    Initial code.

---
+++ log.js
@@ -0,0 +1,32 @@
+var request = require('request')
+  , sys = require('sys')
+  , irc = require('./IRC-js/lib/irc')
+  ;
+  
+var ircOptions = { server: 'irc.freenode.net'};
+var couchOptions = { uri:'http://mikeal.couchone.com/couchdbirc'
+                   , method:'POST'
+                   , headers:{'content-type':'application/json'}
+                   }
+                   ;
+
+var bot = new irc(ircOptions);
+bot.connect(function () {
+  bot.nick('couchlog');
+  bot.join('#couchdb');
+  bot.addListener('privmsg', function (message) {
+    if (message.params.length === 2) {
+      message.channel = message.params[0];
+      message.message = message.params[1];
+      delete message.params
+    }
+    if (message.channel === '#couchdb') {
+      message.timestamp = new Date();
+      couchOptions.body = JSON.stringify(message);
+      couchOptions.headers['content-length'] = couchOptions.body.length;
+      request(couchOptions, function (err, resp, body) {
+        sys.puts(body);
+      })
+    }
+  })
+});
